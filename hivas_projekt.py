7bc52adf09eab5cb9af1f559c8f006cd7cb6f9ce
Rbencuska69 <116081300+Bence38@users.noreply.github.com>
2024-03-20 19:21:28 +0100

    2 és a 3. feladat fele

--- hivas_projekt.py
+++ hivas_projekt.py
@@ -1,8 +1,10 @@
 def mpbe(o, p, mp):
     return o * 60 * 60 + p * 60 + mp
 hivasok = []
+stat = {}
 f = open('hivas.txt','rt', encoding="utf-8")
 for sor in f :
     sor = sor.strip().split(" ")
     print(sor)
+    if sor[0] in stat.items():
 
