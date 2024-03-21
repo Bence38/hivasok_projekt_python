e9ba420c343224791c185d88ccb534a5c9714ab0
Rbencuska69 <116081300+Bence38@users.noreply.github.com>
2024-03-21 15:48:43 +0100

    4.feladat nem teljesen jó

--- hivas_projekt.py
+++ hivas_projekt.py
@@ -16,5 +16,16 @@ for hivas1 in hivasok:
         stat[hivas1[0]] = 1
 for k, v in stat.items():
     print(f'{k} óra {v} hívás')
-print(stat)
+
+print('4.feladat')
+max = 0
+m_sorszam = 0
+h_sorszam = 0
+for hivas1 in hivasok:
+    h_hossz = mpbe(hivas1[0], hivas1[1], hivas1[2])+ mpbe(hivas1[3], hivas1[4], hivas1[5])
+    if max < h_hossz:
+        max = h_hossz
+        m_sorszam = h_sorszam
+    h_sorszam += 1
+print(f'A leghosszabb ideig vonalban lévő {max} ,sorban szerepel,a hivas hossza:{h_hossz} masodperc')
 
