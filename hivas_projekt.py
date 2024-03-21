2229d1eda822effcc3e5e57b0859bef367bc67fb
Rbencuska69 <116081300+Bence38@users.noreply.github.com>
2024-03-21 15:15:02 +0100

    3.feladat kész

--- hivas_projekt.py
+++ hivas_projekt.py
@@ -5,6 +5,16 @@ stat = {}
 f = open('hivas.txt','rt', encoding="utf-8")
 for sor in f :
     sor = sor.strip().split(" ")
-    print(sor)
-    if sor[0] in stat.items():
+    hivasok.append(sor)
+    #print(sor)
+
+print('3.feladat')
+for hivas1 in hivasok:
+    if  hivas1[0] in stat.keys():
+        stat[hivas1[0]] += 1 
+    else:
+        stat[hivas1[0]] = 1
+for k, v in stat.items():
+    print(f'{k} óra {v} hívás')
+print(stat)
 
